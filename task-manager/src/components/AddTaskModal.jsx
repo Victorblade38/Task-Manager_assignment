cdb753200530a96f97ccc3e2e273a1f8bfec109d
prathammhavale <prathammhavale38@gmail.com>
2024-11-17 00:52:48 +0530

    .

---
+++ task-manager/src/components/AddTaskModal.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { useTask } from "../context/Todocontext";
+
+const AddTaskModal = ({ isOpen, onClose }) => {
+  const initialTaskState = {
+    title: "",
+    description: "",
+    priority: 4,
+    completed: false,
+  };
+
+  const [task, setTask] = useState(initialTaskState);
+
+  const { addTask } = useTask();
+
+  const add = (e) => {
+    e.preventDefault();
+    if (!task.title.trim() || !task.description.trim()) {
+      alert("Please provide both title and description!");
+      return;
+    }
+    addTask({ id: Date.now(), ...task });
+    setTask(initialTaskState);
+    onClose();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTask((prevTask) => ({ ...prevTask, [name]: value }));
+  };
+
+  return isOpen ? (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+      role="dialog"
+      aria-labelledby="add-task-title"
+      aria-modal="true"
+    >
+      <div className="bg-white rounded-lg shadow-lg p-6 w-[500px]">
+        <h2 id="add-task-title" className="text-xl font-bold mb-4">
+          Add New Task
+        </h2>
+        <form onSubmit={add} className="flex flex-col gap-3 font-medium">
+          <label htmlFor="title" className="text-lg">
+            Enter the title
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            maxLength={22}
+            placeholder="write a task within 50 letters"
+            className="font-normal w-full h-14  border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+            onChange={handleChange}
+            value={task.title}
+          />
+          <label htmlFor="description">Enter the description</label>
+          <textarea
+            id="description"
+            name="description"
+            maxLength={150}
+            placeholder="write the description within 200 characters"
+            className="font-normal w-full h-20 border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+            onChange={handleChange}
+            value={task.description}
+          />
+          <label htmlFor="priority">Set priority</label>
+          <input
+            type="number"
+            id="priority"
+            name="priority"
+            min="1"
+            max="5"
+            className="w-full border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+            onChange={handleChange}
+            value={task.priority}
+          />
+          <div className="flex justify-end gap-2 mt-4">
+            <button
+              type="button"
+              className="text-l px-4 bg-gray-300 hover:text-white hover:bg-red-500 transition-colors ease-in duration-200 rounded-lg "
+              onClick={onClose}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="text-lg px-4 py-2 bg-green-600 active:bg-green-700 text-white rounded-lg"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default AddTaskModal;
---
+++ task-manager/src/components/Task.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { useTask } from "../context/Todocontext";
+import { deleteIcon, medalIcon } from "../assets";
+import confetti from "canvas-confetti";
+
+const Task = ({ task }) => {
+  const { deleteTask, toggleComplete } = useTask();
+  const [isFading, setIsFading] = useState(false);
+  const { title, description, priority, completed } = task;
+
+  const launchConfetti = () => {
+    confetti({
+      particleCount: 200,
+      spread: 80,
+      angle: 210,
+      origin: { x: 0.9, y: 0.1 },
+      colors: ["#ff0000", "#00ff00", "#0000ff"],
+    });
+  };
+
+  const deleteTaskHandler = () => {
+    setIsFading(true);
+    setTimeout(() => {
+      deleteTask(task.id);
+    }, 500);
+  };
+
+  const toggleHandler = (e) => {
+    toggleComplete(task.id);
+    if (e.target.checked) {
+      launchConfetti();
+    }
+  };
+
+  return (
+    <div
+      className={`w-96 h-64 bg-white  flex flex-col justify-center gap-2 p-6 rounded-lg shadow-lg transition-opacity duration-500 ${
+        isFading ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      <p
+        className={`w-full h-12  text-3xl font-bold ${
+          completed ? "line-through text-slate-600" : "text-slate-800"
+        }`}
+      >
+        {title}
+      </p>
+      <hr />
+      <p
+        className={` w-full h-24 row-span-2 font-medium  ${
+          completed ? "line-through text-slate-500" : "text-slate-700"
+        }`}
+      >
+        {description}
+      </p>
+
+      <div
+        className=" row-span-1 flex flex-row justify-between
+       items-center gap-4 px-2"
+      >
+        <div className="flex flex-row items-center gap-4">
+          <button
+            onClick={deleteTaskHandler}
+            className=" hover:bg-yellow-100 p-2 rounded-md"
+          >
+            <img src={medalIcon} alt="delete icon" className="w-6 h-6" />
+          </button>
+          <p className=" text-xl font-medium text-slate-600">{priority}</p>
+        </div>
+        <div className="flex flex-row  items-center gap-3">
+          <input type="checkbox" className="w-5 h-5" onChange={toggleHandler} />
+          <button
+            onClick={deleteTaskHandler}
+            className=" hover:bg-red-100 active:bg-red-200 p-2 rounded-md"
+          >
+            <img src={deleteIcon} alt="delete icon" className="w-5 h-5" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Task;
